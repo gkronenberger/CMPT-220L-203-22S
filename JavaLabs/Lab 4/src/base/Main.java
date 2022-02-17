c17ca7b1799aeb402ed72776488582e4807387ae
gkronenberger <98132570+gkronenberger@users.noreply.github.com>
2022-02-17 16:36:54 -0500

    Lab 4

--- JavaLabs/Lab 4/src/base/Main.java
+++ JavaLabs/Lab 4/src/base/Main.java
@@ -1,5 +1,7 @@
 package base;
 
+import java.util.List;
+
 public class Main {
     /**
      * # Lab Four
@@ -14,10 +16,34 @@ public class Main {
      * - Combining functions
      * - Structuring solutions
      */
+    static int value1 = 2;
+    static int value2 = 7;
+    static int[] nums = {0,1,2,3,4,5,6,7,8,9};
     public static void main(String[] args) {
         // Give me an example of you using switch case.
+        switch(value1){
+            case 1:
+                System.out.println("The number is 1.");
+                break;
+            case 2:
+                System.out.println("The number is 2.");
+                break;
+            case 3:
+                System.out.println("The number is 3");
+                break;
+            default:
+                System.out.println("The number isn't in the range.");
+                break;
+        }
         // Give me an example of you using a for loop
+        for (int x = 1; x < 7; x++) {
+            System.out.println("The value is" + x);
+        }
         // Give me an example of you using a while loop
+        while (value1 < 11) {
+            System.out.println("The value is" + value1);
+            value1 = value1++;
+        }
     }
     // For the following todos you'll probably want to define them below and then call them from the main function above.
 
@@ -26,18 +52,42 @@ public class Main {
      * For example if given the numbers 2 and 3. The math that the function should do is 2^3
      * and should print out or return 8. Print the output.
      */
+    public static int raise(int value1, int value2) {
+        int pow = (int) Math.pow(value1, value2);
+        return pow;
+    }
 
     /**
      * I want you to write a function that will take in a list and see how many times a given number is in the list.
      * For example if the array given is [2,3,5,2,3,6,7,8,2] and the number given is 2 the function should print out
      * or return 3. Print the output.
      */
+    public static int listcount(int value1, int [] nums) {
+        int numcount = 0;
+        for (int i = 0; i < nums.length; i++)
+            {
+                if (value1 == nums[i])
+                {
+                   numcount = numcount + 1;
+                }
+            }
+        return numcount;
+    }
+
 
     /**
      * Create a function named summation that will take in an int[ ] and return an int.
      * I want the function to then go through the given array and sum up digits in the array.
      * Once that’s done print or return the answer. Print the output
      */
+    public static int summation(int [] nums) {
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) 
+        {
+            total = total + nums[i];
+        }
+        return total;
+    }
 
     /**
      * Give me a function that gives the answer to the pythagorean theorem.
@@ -50,9 +100,16 @@ public class Main {
      *
      * Once that’s done print or return the answer. Print the output
      */
+    public static int pytheorem(int value1, int value2) {
+        double a2 = Math.pow(value1, 2);
+        double b2 = Math.pow(value2, 2);
+        double sum = a2 + b2;
+        double c = Math.sqrt(sum);
+        return (int) c;
+    }
 
-    int addition(int a, int b) {
-        int answer = a + b;
+    int addition(int a2, int b2) {
+        int answer = a2 + b2;
         return answer;
     }
 
